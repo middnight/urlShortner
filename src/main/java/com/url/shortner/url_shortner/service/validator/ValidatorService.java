882f540d8b7a1202dc6ea81b38cd5c33eb79108e
shivam <shivamgiri420@gmail.com>
2024-06-23 18:01:31 +0530

    added validation layer, not completed

---
+++ src/main/java/com/url/shortner/url_shortner/service/validator/ValidatorService.java
@@ -0,0 +1,5 @@
+package com.url.shortner.url_shortner.service.validator;
+
+public interface ValidatorService {
+  void validateUrl(String url);
+}
---
+++ src/main/java/com/url/shortner/url_shortner/service/validator/ValidatorServiceImpl.java
@@ -0,0 +1,9 @@
+package com.url.shortner.url_shortner.service.validator;
+
+public class ValidatorServiceImpl implements ValidatorService {
+  @Override
+  public void validateUrl(String url) {
+    // java does not have a standard way to validate url :(
+    return;
+  }
+}
